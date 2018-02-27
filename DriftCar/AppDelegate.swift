e5a647bb08b7a6748475c9109249edd6b1c7e541
Krystian Śliwa <krystian.sliwa@gmail.com>
2018-02-27 15:19:38 +0100

    Initial Commit

---
+++ DriftCar/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  DriftCar
+//
+//  Created by Krystian Śliwa on 27/02/2018.
+//  Copyright © 2018 Krystian Śliwa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
